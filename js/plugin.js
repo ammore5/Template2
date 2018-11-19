5e64b374d94aa41c2aded0ddc48ac1ae8d550a83
Ammar Alnouirah <alnouirah@gmail.com>
2018-11-19 19:12:06 +0300

    add Greate services Section

--- js/plugin.js
+++ js/plugin.js
@@ -23,4 +23,42 @@ $('.slider__item').on('mouseover', function(){
  function wow(){
   new WOW().init();
 }
+
+$('.icon-arrow-down').click(function(){
+  $('html,body').animate({
+    scrollTop:$('.Partners').offset().top
+  },1400);
+});
+
+
+$('a[href="news"]').click(function(event){
+  event.preventDefault();
+  $('html,body').animate({
+    scrollTop:$('.prices').offset().top
+  },1400);
+});
+
+
+$('a[href="teamWork"]').click(function(event){
+  event.preventDefault();
+  $('html,body').animate({
+    scrollTop:$('.ServicesEdit').offset().top
+  },1400);
+});
+
+
+$('a[href="PROFILE"]').click(function(event){
+  event.preventDefault();
+  $('html,body').animate({
+    scrollTop:$('.protfolio').offset().top
+  },1400);
+});
+
+
+let carouselPage = "";
+$(".ServButCss").on("click",function(){
+  carouselPage =  $(this).data("value");
+  $(".skw-page-" + carouselPage).addClass("active").siblings().removeClass("active");
+})
+
 });
