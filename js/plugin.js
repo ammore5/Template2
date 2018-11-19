d611c5eccad8d3f240c648e43217eedcf806e98e
Ammar Alnouirah <alnouirah@gmail.com>
2018-11-19 11:08:10 +0300

    Edit the Main pic background

--- js/plugin.js
+++ js/plugin.js
@@ -1,3 +1,12 @@
+$( document ).ready(function() {
+
+  setTimeout(function(){
+    $('.loadingOverlay').fadeOut(800,function(){
+      $(this).remove();
+    })
+  }, 1000);
+
+
 $('.slider__item').on('mouseover', function(){
     $('.slider__item').removeClass('active');
     $(this).addClass('active');
@@ -6,3 +15,7 @@ $('.slider__item').on('mouseover', function(){
     $('.slider__item').removeClass('active');
     $(this).addClass('');
   });
+
+  $('.mainCarousel').height($(window).innerHeight());
+ 
+});
