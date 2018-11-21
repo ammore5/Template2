e2a8597bf742ff7f12c69f4c0f965f3be5bddfc6
Ammar Alnouirah <alnouirah@gmail.com>
2018-11-21 10:50:00 +0300

    finish all the work

--- js/plugin.js
+++ js/plugin.js
@@ -1,13 +1,48 @@
 /* $,document */
 $( document ).ready(function() {
 
-    $('.loadingOverlay').fadeOut(800,function(){
-      $(this).remove();
-    })
+    setTimeout(function(){
+      $('.loadingOverlay').fadeOut(800,function(){
+        $(this).remove();
+      })
+    }, 1000);
 
     wow();
 
+  $(window).scroll(function(){
+    if(window.scrollY > 600){
+      $('.upBottun').fadeIn();
+    }else{
+      $('.upBottun').fadeOut();
+    }
+  });
 
+  $('.upBottun').on('click',function(){
+    $('html,body').animate({
+      scrollTop:$('.mainCarousel').offset().top
+    },1400);
+  });
+
+  $('a[href="TimeLine"]').on('click',function(event){
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop:$('.timeLine').offset().top
+    },1400);
+  });
+
+  $('a[href="ContactUs"]').on('click',function(event){
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop:$('.statistecs').offset().top
+    },1400);
+  });
+  
+  $('a[href="Addons"]').on('click',function(event){
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop:$('.addons').offset().top
+    },1400);
+  });
 
 $('.slider__item').on('mouseover', function(){
     $('.slider__item').removeClass('active');
@@ -61,4 +96,26 @@ $(".ServButCss").on("click",function(){
   $(".skw-page-" + carouselPage).addClass("active").siblings().removeClass("active");
 })
 
+
+
+/*----- Make Product slider change automatically */
+// let caroCount = 1;
+// function caroSlide(){
+//   $(".skw-page-" + caroCount).addClass("active").siblings().removeClass("active");
+//   if(caroCount == 4){
+//     caroCount = 1;
+//   }else{
+//     caroCount = caroCount + 1;
+//   }
+
+//   caroSlideTimer();
+// }
+
+// function caroSlideTimer(){
+//   setTimeout(function(){
+//     caroSlide();
+//   }, 5000);
+// }
+
+// caroSlideTimer();
 });
